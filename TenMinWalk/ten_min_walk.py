31d2853fae4b17a84273d63b99712c039c595429
synaplasticity <synaplasticity@gmail.com>
2018-03-04 11:37:37 +0530

    Better version using list count

--- TenMinWalk/ten_min_walk.py
+++ TenMinWalk/ten_min_walk.py
@@ -1,25 +1,2 @@
-def toggle_dir(dir):
-    """
-    Returns opposite of the given direction.
-    dir : single char. e.g.: 'n'
-    """
-    direction = {'n': 's', 's': 'n', 'e': 'w', 'w': 'e'}
-
-    return direction[dir]
-
-
 def isValidWalk(walk_dir):
-    len_walk_dir = len(walk_dir)
-    if len_walk_dir is not 10:
-        return False
-
-    # while len(walk_dir) > 0:
-    for a in range(1, len_walk_dir - 1):
-        if len(walk_dir) is 0:
-            break
-        opp_dir = toggle_dir(walk_dir[1])
-        if opp_dir in walk_dir:
-            del walk_dir[1]
-            del walk_dir[walk_dir.index(opp_dir)]
-
-    return len(walk_dir) is 0
+    return len(walk_dir) is 10 and walk_dir.count('n') == walk_dir.count('s') and walk_dir.count('e') == walk_dir.count('w')
--- TenMinWalk/test/test_ten_min_walk.py
+++ TenMinWalk/test/test_ten_min_walk.py
@@ -1,21 +1,4 @@
-from TenMinWalk.ten_min_walk import toggle_dir
 from TenMinWalk.ten_min_walk import isValidWalk
-
-
-def test_toogle_dir_north():
-    assert toggle_dir('n') is 's'
-
-
-def test_toogle_dir_south():
-    assert toggle_dir('s') is 'n'
-
-
-def test_toogle_dir_east():
-    assert toggle_dir('e') is 'w'
-
-
-def test_toogle_dir_west():
-    assert toggle_dir('w') is 'e'
 
 
 def test_less_than_ten_mins():
