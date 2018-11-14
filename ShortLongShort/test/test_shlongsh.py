356a7f8032e77f9730669863987ed7bd82470130
synaplasticity <synaplasticity@gmail.com>
2018-11-14 12:19:12 +0530

    Added more tests

--- ShortLongShort/test/test_shlongsh.py
+++ ShortLongShort/test/test_shlongsh.py
@@ -2,5 +2,6 @@ from ShortLongShort.short_long_short import sls
 
 
 def test_first_is_short():
-    print(sls("1", "22"))
     assert sls("1", "22") == "1221"
+def test_second_is_short():
+    assert sls("33", "4") == "4334"
