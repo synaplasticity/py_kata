87172cf7865690293109cddb69f154199d74ad33
synaplasticity <synaplasticity@gmail.com>
2018-12-24 00:35:14 +0530

    Clean up

--- HouseRent/house_rent_calc.py
+++ HouseRent/house_rent_calc.py
@@ -11,7 +11,6 @@ def rent_for_years(init_rent, perc_increase, increase_interval=1, years=1):
     old_rent = init_rent
     for int in range(1, years, increase_interval):
         new_rent = old_rent + old_rent * (perc_increase / 100)
-        print("Old/New : ", int, old_rent)
         old_rent = new_rent
 
     return new_rent
