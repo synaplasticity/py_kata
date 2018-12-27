c74f5a87492394ecdd6b1499eb2619f1a2dfeb5e
synaplasticity <synaplasticity@gmail.com>
2018-12-27 21:30:24 +0530

    Fixed 1 year boundary case

--- HouseRent/house_rent_calc.py
+++ HouseRent/house_rent_calc.py
@@ -1,16 +1,24 @@
 import math
 
 
-def rent_for_years(init_rent, perc_increase, increase_interval=1, years=1):
-    if years == 1:
-        return init_rent
+def rent_per_month(init_rent, perc_increase, increase_interval=1, years=1):
 
     no_of_increases = math.ceil(years / increase_interval)
     print("inc : ", no_of_increases)
+    if no_of_increases == 1:
+        return init_rent
 
-    old_rent = init_rent
+    old_rent = new_rent = init_rent
     for int in range(1, years, increase_interval):
         new_rent = old_rent + old_rent * (perc_increase / 100)
         old_rent = new_rent
 
     return round(new_rent, 2)
+
+
+def annual_rent_for_period(init_rent, perc_increase, increase_interval=1,
+                           years=1):
+    return round(rent_per_month(init_rent, perc_increase, increase_interval,
+                 years) * 12
+                 * years,
+                 2)
--- HouseRent/test/test_houserent_calc.py
+++ HouseRent/test/test_houserent_calc.py
@@ -1,21 +1,31 @@
-from HouseRent.house_rent_calc import rent_for_years
+from HouseRent.house_rent_calc import rent_per_month
+from HouseRent.house_rent_calc import annual_rent_for_period
 
 
-def test_rent_for_3year_interval():
-    assert rent_for_years(35000, 1.5) == 35000
+def test_rent_for_1year_interval():
+    assert rent_per_month(35000, 1.5) == 35000
+
+
+def test_rent_for_3year_interval_for_3years():
+    assert rent_per_month(35000, 1.5, increase_interval=3, years=3) == 35000
 
 
 def test_rent_for_3year_interval_for_4years():
-    assert rent_for_years(35000, 15, increase_interval=3, years=4) == 40250
+    assert rent_per_month(35000, 15, increase_interval=3, years=4) == 40250
 
 
 def test_rent_for_3year_interval_for_7years():
-    assert rent_for_years(35000, 15, increase_interval=3, years=7) == 46287.5
+    assert rent_per_month(35000, 15, increase_interval=3, years=7) == 46287.5
 
 
 def test_rent_for_3year_interval_for_10years():
-    assert rent_for_years(35000, 15, increase_interval=3, years=10) == 53230.62
+    assert rent_per_month(35000, 15, increase_interval=3, years=10) == 53230.62
 
 
 def test_rent_for_3year_interval_for_16years():
-    assert rent_for_years(35000, 15, increase_interval=3, years=16) == 70397.50
+    assert rent_per_month(35000, 15, increase_interval=3, years=16) == 70397.50
+
+
+def test_annual_rent_for_3years():
+    assert (annual_rent_for_period(35000, 15, increase_interval=3, years=3) ==
+            35000 * 12 * 3)
